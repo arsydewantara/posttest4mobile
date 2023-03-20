65994548265ed3448bae5f50a087d38cdb440788
arsydewantara <jendralsudirman7@gmail.com>
2023-03-20 22:45:41 +0800

    posttest 4

---
+++ posttest4_033_arsy/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:posttest4_033_arsy/mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(scaffoldBackgroundColor: Color.fromARGB(255, 3, 216, 253)),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Muhammad Arsy Dewantara"),
+        backgroundColor: Color.fromARGB(249, 223, 191, 9),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          Container(
+            height: 500,
+            width: 500,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/logo.png"))),
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Color.fromARGB(249, 223, 191, 9)),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (value) {
+                return MainPage();
+              }));
+            },
+            child: Text(
+              "Ayo Top Up",
+              style: TextStyle(color: Color.fromARGB(255, 20, 3, 78)),
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
